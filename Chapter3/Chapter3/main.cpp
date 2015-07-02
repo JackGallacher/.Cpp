4093f655c445f56bf21d3ad461ca76ce25bc9550
Jack Gallacher <jackleslie.gallacher@gmail.com>
2015-07-02 19:19:28 +0100

    Chapter 3 Tasks
    Outputs a string value, adds two numbers and divides two numbers.

---
+++ Chapter3/Chapter3/main.cpp
@@ -0,0 +1,30 @@
+using namespace std;
+#include <iostream>;
+
+int main()
+{
+	//1
+	cout << "Jack";
+
+	//2
+	double x = 0;
+	double y = 0;
+
+	cout << "input number 1: ";
+	cin >> x;
+	cout << "input number 2: ";
+	cin >> y;
+	cout << "input 1 + input 2 = " << x + y << "\n";
+
+	//3
+	double i = 0;
+	double j = 0;
+
+	cout << "input number 1: ";
+	cin >> i;
+	cout << "input number 2: ";
+	cin >> j;
+	cout << "input 1 / input 2 = " << i / j << "\n";
+
+	system("pause");
+}
