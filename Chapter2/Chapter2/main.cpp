4e829fd11242c6db1345b620e3bc1f09331a7553
Jack Gallacher <jackleslie.gallacher@gmail.com>
2015-07-02 19:12:13 +0100

    Chapter 2 Tasks
    This program just outputs a few string values.

---
+++ Chapter2/Chapter2/main.cpp
@@ -0,0 +1,10 @@
+using namespace std;
+#include <iostream>
+
+int main()
+{
+	cout << "My name is Jack \n";//1
+	cout << "Ben \n" << "Anna \n" << "Louise \n" << "Nadia \n" << "Ryan \n";//2
+
+	system("pause");
+}
