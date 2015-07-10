21c5a9a110c1246ff8fbe903b6f0dfd1958735aa
Jack Gallacher <jackleslie.gallacher@gmail.com>
2015-07-10 11:00:00 +0100

    Chapter 8 Poker change.
    changed the card array to a vector for dynamic removal of cards when
    they have been drawn.

--- Chapter8/Chapter8/main.cpp
+++ Chapter8/Chapter8/main.cpp
@@ -3,12 +3,14 @@ using namespace std;
 #include <string>
 #include <ctime>
 #include <cstdlib>
+#include <vector>
+#include <algorithm>
 
 //1 - write a program that simulates a coinflip.
 //2 - write a program that picks a number between 0 and 100 and lets the user guess the number, the programs says "too high" "too low" or "just right" based on the closeness of the guess.
 //3 - write a program that solves the guessing game.
 //4 - make a "slot machine" that randomly displays the results of the machine to the player, have three or more values for each wheel and print out winnings if the values match.
-//5 - write a program to play poker, give 5 cards to the player, let the player choose new cards and determine how good the hand is.
+//5 - write a program to play poker, give 5 cards to the player, let the player choose new cards and determine how good the hand is. keep track of the cards that heve been drawn, and dont allow them to be drawn again.
 
 
 void menu();//prototype of menu.
@@ -121,16 +123,48 @@ void slots()
 		if (numbers[0] == 1 && numbers[1] == 1 && numbers[2] == 1)
 		{
 			cout << "all the 1`s matched\n";
+			menu();
 		}
 		if (numbers[0] == 2 && numbers[1] == 2 && numbers[2] == 2)
 		{
 			cout << "all the 2's matched\n";
+			menu();
 		}
 		if (numbers[0] == 3 && numbers[1] == 3 && numbers[2] == 3)
 		{
 			cout << "all the 3's matched\n";
+			menu();
 		}
 	}
+}
+void gethand()
+{
+	vector<string> deck;//ajustable vector for the cards as 
+	string names[13] = { "Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King" };
+	string house[4] = { "Clubs", "Spades", "Hearts", "Diamonds" };
+
+	int i = 0;//array position.
+	for (int j = 0; j < 13; j++)//names slot
+	{
+		for (int k = 0; k < 4; k++)//house slot
+		{
+			deck.push_back(names[j] + " of " + house[k]); //= names[j] + " of " + house[k];
+			//cout << deck[i] << "\n";
+			i++;
+		}
+	}
+	cout << "your hand is: ";
+	for (int i = 0; i < 5; i ++)//outputs the players hand, each card is based upon a random number between 1 and 52.
+	{
+		string selectedcard = deck[getrandom(1, 52)];
+		cout << selectedcard << "\n";
+		//todo - delete card from vector so that if the hand is redrawn, these current cards will not chosen again.
+	}
+}
+void poker()
+{
+	cout << "welcome to poker";
+	gethand();
 }
 //switch selection of tasks.
 void menu()
@@ -141,6 +175,7 @@ void menu()
 	cout << "press 2 to play the guessing game\n";
 	cout << "press 3 to automatically solve the guessing game\n";
 	cout << "press 4 to play slots\n";
+	cout << "press 5 to play poker\n";
 	cin >> user_input;
 
 	switch (user_input)
@@ -156,6 +191,9 @@ void menu()
 		break;
 	case 4:
 		slots();
+		break;
+	case 5:
+		poker();
 		break;
 	default:
 		cout << "you did not select a valid option, please try again \n";
