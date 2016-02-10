63718bffc07e22be6cea2dd7f6018d2a3d94861f
Jack Gallacher <jackleslie.gallacher@gmail.com>
2016-02-10 20:23:50 +0000

    Fixed linked list program
    Made the linked list program work, I understood how to do it this time.

--- Jumping into C++/15 - Linked Lists/15 - Linked Lists/Source.cpp
+++ Jumping into C++/15 - Linked Lists/15 - Linked Lists/Source.cpp
@@ -1,52 +1,62 @@
 using namespace std;
 #include <cstddef>//used to allow the use of NULL.
 #include <iostream>
+#include <string>
 
 struct spaceship//creates a spaceship object with the following parameters.
 {
+	string name;
 	int x_position;
 	int y_position;
 	int power;
 	spaceship* next_spaceship;//a pointer to the next spaceship in the list. Think of it as a hook on a train. you might not need it, but it is there if you need to hook(create) another carrage.
 };
-//spaceship* fleet = NULL;// this is the list where we will store out list of ships. a "one stop point" for doing things with our list. the ROOT node.
-
-spaceship* new_spaceship(spaceship* fleet)//instead of declaring the head of the list as global, we can just pass it into the function.
+spaceship* add_spaceship(spaceship* ship_list, string ship_name)
 {
-	spaceship* ship = new spaceship;//creates an instane of the spaceship struct that we will be the return value.
-	ship->x_position = 0;//the "->" allows you to acces the feilds of a structure through a pointer.
+	spaceship* ship = new spaceship();//creates a new instance of a spaceship that we are going to add to our linked list.
+	ship->name = ship_name;
+	ship->x_position = 0;
 	ship->y_position = 0;
 	ship->power = 20;
-	ship->next_spaceship = fleet;//makes this ship points to previous ship/first element in the list.
-	//fleet = ship;//makes the current ship the first object in the list, we dont need this when passing the head of the list directly into the function.
-	return ship;//returns a newly created spaceship structure.
+	ship->next_spaceship = ship_list;//assigns the next_sapceship variable to the previous element in the list, "linking" them.
+	return ship;
 }
-
-void upgrade_power(spaceship* ship)//takes in the pointer of a spaceship object location and adds to its power variable.
+void upgrade_power(spaceship* ship_list)//takes in the pointer of a spaceship object location and adds to its power variable.
 {
-	ship->power += 10;
+	spaceship* current_ship = ship_list;
+	while (current_ship != NULL)
+	{
+		current_ship->power + 10;
+		current_ship = current_ship->next_spaceship;
+	}
+
 }
 
+void display_linkedlist(spaceship* ship_list)
+{
+	spaceship* current_ship = ship_list;
+	while (current_ship != NULL)
+	{
+		cout << "ship name: "<< current_ship->name << "\n";
+		cout << "ship power: " << current_ship->power << "\n";
+		current_ship = current_ship->next_spaceship;
+	}
+}
 void display_ship(spaceship &ship)
 {
 	cout << "\npower: " << ship.power;
 	cout << "\nx_position: " << ship.x_position;
-	cout << "\ny_position" << ship.y_position + "\n";
+	cout << "\ny_position " << ship.y_position << "\n";
 }
-
 int main()
 {	
-	//spaceship* my_ship = new_spaceship();//creates a pointer of a new spaceship object.
-	spaceship* my_ship = NULL;//to do it without a global, a new enemy is created as null.
-	spaceship* my_ship2 = NULL;
+	spaceship* ship_list = NULL;//creates out list that we will add stuff to.
+	ship_list = add_spaceship(ship_list, "ship 1");//here we specify the linked list we are adding to, plus passing in the name of the node we are adding.
+	ship_list = add_spaceship(ship_list, "ship 2");
+	ship_list = add_spaceship(ship_list, "ship 3");
 
-	my_ship = new_spaceship(my_ship);//it is then passed into the list and made the end point of the list until a new object is added.
-	my_ship2 = new_spaceship(my_ship2);
-
-	upgrade_power(my_ship);//upgrades the power of ship 1.
-
-	display_ship(*my_ship);
-	display_ship(*my_ship2);
+	upgrade_power(ship_list);
+	display_linkedlist(ship_list);
 
 	system("pause");
 }
